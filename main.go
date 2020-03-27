55a0b9660b76d75d2cd0b07cd80783a4b3060610
Steeve PAYRAUDEAU <spayraudeau@easilys.com>
2020-03-27 10:38:18 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"time"
+)
+
+type carboneSDK struct {
+	apiAccessToken string
+	apiURL string
+}
+
+// addTemplate upload your template to Carbone Render.
+// It responds a template ID.
+func (csdk carboneSDK) addTemplate(templateFileName string) {
+	// Create buffer
+	buf := new(bytes.Buffer)
+	// create a tmpfile and assemble your multipart from there
+	w := multipart.NewWriter(buf)
+
+	// Create the data to send
+	// {
+	//		"payload":"",
+	// 		"template": readstream(file...)
+	// }
+	label, err := w.CreateFormField("payload")
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	// Write payload content (empty for now)
+	label.Write([]byte(""))
+
+	fw, err := w.CreateFormFile("template", templateFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Open Template
+	fd, err := os.Open(templateFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fd.Close()
+	// Write file field from file to upload
+	_, err = io.Copy(fw, fd)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Important if you do not close the multipart writer you will not have a terminating boundry
+	w.Close()
+
+	req, err := http.NewRequest("POST", csdk.apiURL, buf)
+	if err != nil {
+		log.Fatal("Error POST request:", err)
+	}
+
+	// Set headers
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	req.Header.Set("Authorization", csdk.apiAccessToken)
+
+	// Set client timeout
+	client := &http.Client{Timeout: time.Second * 10}
+
+	// Send request
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error reading response. ", err)
+	}
+	defer resp.Body.Close()
+
+	// fmt.Println("response Status:", resp.Status)
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("bad status: %s", resp.Status)
+		fmt.Println("response Headers:", resp.Header)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading body. ", err)
+	}
+	fmt.Printf("%s\n", body)
+}
+
+func main() {
+	csdk := carboneSDK{
+		apiURL: "https://render.carbone.io/template"
+		apiAccessToken: "eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNjY3IiwiYXVkIjoiY2FyYm9uZSIsImV4cCI6MjIwNzQwNjQ0NywiZGF0YSI6eyJpZEFjY291bnQiOjE2Njd9fQ.AH2NiPdd8dRC_FNsd4aJ1DHy2wNNhXFmRvyh6PM-jkksfPn7hIIgiUfZ-L7Ng9Jou3eCeLrymjcPuABFVcaGiGvCATAICKX_j7WKBdMO_iPzD1LvL5j35FX1_i513OLqSvqTY_3KvBZO2RXMh4tLWlMn-dhNFLn-aE6IcS3lpce_A2PB"
+	}
+	templateFileName := "./template.odt"
+
+	csdk.addTemplate(templateFileName)
+}
+
+// templateId: f90e67221d7d5ee11058a000bdb997fb41bf149b1f88b45cb1aba9edcab8f868
+
+// ======= Log cookie and headers are attached
+// fmt.Println(req.Cookies())
+// fmt.Println(req.Header)
+// fmt.Println(req.Body)
+// ======= Create a file to debug
+// by, e := ioutil.ReadAll(req.Body)
+// if e != nil {
+// 	log.Fatal(e)
+// }
+// err = ioutil.WriteFile("http.log", by, 0644)
+// if err != nil {
+// 	log.Fatal(err)
+// }
