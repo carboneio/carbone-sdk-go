46d7b99d2c9fad6ea1163a1fc28227007fd3ad85
Steeve PAYRAUDEAU <spayraudeau@easilys.com>
2020-03-27 11:44:27 +0100

    Parse JSON response and fill response structs

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -12,13 +13,24 @@ import (
 	"time"
 )
 
+type carboneResponseData struct {
+	TemplateID string `json:"templateId,omitempty"`
+	RenderID   string `json:"renderId,omitempty"`
+}
+
+type carboneResponse struct {
+	Success bool                `json:"success"`
+	Error   string              `json:"error,omitempty"`
+	Data    carboneResponseData `json:"data"`
+}
+
 type carboneSDK struct {
 	apiAccessToken string
 	apiURL         string
 }
 
 // addTemplate upload your template to Carbone Render.
-func (csdk carboneSDK) addTemplate(templateFileName string, payload string) {
+func (csdk carboneSDK) addTemplate(templateFileName string, payload string) (carboneResponse, error) {
 	fmt.Println("Payload:", payload)
 	// Create buffer
 	buf := new(bytes.Buffer)
@@ -87,7 +99,15 @@ func (csdk carboneSDK) addTemplate(templateFileName string, payload string) {
 		log.Fatal("Error reading body. ", err)
 	}
 	fmt.Printf("%s\n", body)
+	cResp := carboneResponse{}
+	err = json.Unmarshal(body, &cResp)
+	if err != nil {
+		return cResp, err
+	}
+	return cResp, nil
 }
+
+// func
 
 func main() {
 	csdk := carboneSDK{
@@ -95,8 +115,17 @@ func main() {
 		apiAccessToken: "eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNjY3IiwiYXVkIjoiY2FyYm9uZSIsImV4cCI6MjIwNzQwNjQ0NywiZGF0YSI6eyJpZEFjY291bnQiOjE2Njd9fQ.AH2NiPdd8dRC_FNsd4aJ1DHy2wNNhXFmRvyh6PM-jkksfPn7hIIgiUfZ-L7Ng9Jou3eCeLrymjcPuABFVcaGiGvCATAICKX_j7WKBdMO_iPzD1LvL5j35FX1_i513OLqSvqTY_3KvBZO2RXMh4tLWlMn-dhNFLn-aE6IcS3lpce_A2PB",
 	}
 	templateFileName := "./template.odt"
+	cresp, err := csdk.addTemplate(templateFileName, "")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	csdk.addTemplate(templateFileName, "1324")
+	fmt.Println("Success:", cresp.Success)
+	if cresp.Success == false {
+		log.Fatal(cresp.Error)
+	} else {
+		fmt.Printf("%+v", cresp.Data)
+	}
 }
 
 // templateId: f90e67221d7d5ee11058a000bdb997fb41bf149b1f88b45cb1aba9edcab8f868
