b0cf43de8c0e0d158d8b1142ae36463355a83f96
Steeve PAYRAUDEAU <spayraudeau@easilys.com>
2020-03-31 16:23:25 +0200

    added test

--- carbone_test.go
+++ carbone_test.go
@@ -3,8 +3,12 @@ package carbone
 import (
 	"errors"
 	"fmt"
+	"log"
+	"os"
 	"testing"
 )
+
+var csdk CSDK
 
 /**
 	TODO:
@@ -12,11 +16,20 @@ import (
 	- full test: upload template, render report, get the report, delete the template
 **/
 
-func TestAddTemplate(t *testing.T) {
-	csdk, err := NewCarboneSDK("eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNjY3IiwiYXVkIjoiY2FyYm9uZSIsImV4cCI6MjIwNzQwNjQ0NywiZGF0YSI6eyJpZEFjY291bnQiOjE2Njd9fQ.AH2NiPdd8dRC_FNsd4aJ1DHy2wNNhXFmRvyh6PM-jkksfPn7hIIgiUfZ-L7Ng9Jou3eCeLrymjcPuABFVcaGiGvCATAICKX_j7WKBdMO_iPzD1LvL5j35FX1_i513OLqSvqTY_3KvBZO2RXMh4tLWlMn-dhNFLn-aE6IcS3lpce_A2PB")
-	if err != nil {
-		t.Error(err)
+func TestMain(m *testing.M) {
+	var e error
+	csdk, e = NewCarboneSDK("eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNjY3IiwiYXVkIjoiY2FyYm9uZSIsImV4cCI6MjIwNzQwNjQ0NywiZGF0YSI6eyJpZEFjY291bnQiOjE2Njd9fQ.AH2NiPdd8dRC_FNsd4aJ1DHy2wNNhXFmRvyh6PM-jkksfPn7hIIgiUfZ-L7Ng9Jou3eCeLrymjcPuABFVcaGiGvCATAICKX_j7WKBdMO_iPzD1LvL5j35FX1_i513OLqSvqTY_3KvBZO2RXMh4tLWlMn-dhNFLn-aE6IcS3lpce_A2PB")
+	if e != nil {
+		log.Fatal(e)
 	}
+	os.Exit(m.Run())
+}
+
+func TestAddTemplate(t *testing.T) {
+	// csdk, err := NewCarboneSDK("eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNjY3IiwiYXVkIjoiY2FyYm9uZSIsImV4cCI6MjIwNzQwNjQ0NywiZGF0YSI6eyJpZEFjY291bnQiOjE2Njd9fQ.AH2NiPdd8dRC_FNsd4aJ1DHy2wNNhXFmRvyh6PM-jkksfPn7hIIgiUfZ-L7Ng9Jou3eCeLrymjcPuABFVcaGiGvCATAICKX_j7WKBdMO_iPzD1LvL5j35FX1_i513OLqSvqTY_3KvBZO2RXMh4tLWlMn-dhNFLn-aE6IcS3lpce_A2PB")
+	// if err != nil {
+	// 	t.Error(err)
+	// }
 	resp, err := csdk.AddTemplate("./template.odt", "")
 	if err != nil {
 		t.Error(err)
@@ -27,14 +40,14 @@ func TestAddTemplate(t *testing.T) {
 	if len(resp.Data.TemplateID) <= 0 {
 		t.Error(errors.New("templateId not returned from the api"))
 	}
-	fmt.Println(resp.Data.TemplateID)
+	// fmt.Println(resp.Data.TemplateID)
 }
 
 func TestGetTemplate(t *testing.T) {
-	csdk, err := NewCarboneSDK("eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNjY3IiwiYXVkIjoiY2FyYm9uZSIsImV4cCI6MjIwNzQwNjQ0NywiZGF0YSI6eyJpZEFjY291bnQiOjE2Njd9fQ.AH2NiPdd8dRC_FNsd4aJ1DHy2wNNhXFmRvyh6PM-jkksfPn7hIIgiUfZ-L7Ng9Jou3eCeLrymjcPuABFVcaGiGvCATAICKX_j7WKBdMO_iPzD1LvL5j35FX1_i513OLqSvqTY_3KvBZO2RXMh4tLWlMn-dhNFLn-aE6IcS3lpce_A2PB")
-	if err != nil {
-		t.Error(err)
-	}
+	// csdk, err := NewCarboneSDK("eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNjY3IiwiYXVkIjoiY2FyYm9uZSIsImV4cCI6MjIwNzQwNjQ0NywiZGF0YSI6eyJpZEFjY291bnQiOjE2Njd9fQ.AH2NiPdd8dRC_FNsd4aJ1DHy2wNNhXFmRvyh6PM-jkksfPn7hIIgiUfZ-L7Ng9Jou3eCeLrymjcPuABFVcaGiGvCATAICKX_j7WKBdMO_iPzD1LvL5j35FX1_i513OLqSvqTY_3KvBZO2RXMh4tLWlMn-dhNFLn-aE6IcS3lpce_A2PB")
+	// if err != nil {
+	// 	t.Error(err)
+	// }
 	templateData, err := csdk.GetTemplate("f90e67221d7d5ee11058a000bdb997fb41bf149b1f88b45cb1aba9edcab8f868")
 	if err != nil || len(templateData) <= 0 {
 		t.Error(err)
@@ -42,10 +55,10 @@ func TestGetTemplate(t *testing.T) {
 }
 
 func TestAddDeleteTwiceTemplate(t *testing.T) {
-	csdk, err := NewCarboneSDK("eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNjY3IiwiYXVkIjoiY2FyYm9uZSIsImV4cCI6MjIwNzQwNjQ0NywiZGF0YSI6eyJpZEFjY291bnQiOjE2Njd9fQ.AH2NiPdd8dRC_FNsd4aJ1DHy2wNNhXFmRvyh6PM-jkksfPn7hIIgiUfZ-L7Ng9Jou3eCeLrymjcPuABFVcaGiGvCATAICKX_j7WKBdMO_iPzD1LvL5j35FX1_i513OLqSvqTY_3KvBZO2RXMh4tLWlMn-dhNFLn-aE6IcS3lpce_A2PB")
-	if err != nil {
-		t.Error(err)
-	}
+	// csdk, err := NewCarboneSDK("eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIxNjY3IiwiYXVkIjoiY2FyYm9uZSIsImV4cCI6MjIwNzQwNjQ0NywiZGF0YSI6eyJpZEFjY291bnQiOjE2Njd9fQ.AH2NiPdd8dRC_FNsd4aJ1DHy2wNNhXFmRvyh6PM-jkksfPn7hIIgiUfZ-L7Ng9Jou3eCeLrymjcPuABFVcaGiGvCATAICKX_j7WKBdMO_iPzD1LvL5j35FX1_i513OLqSvqTY_3KvBZO2RXMh4tLWlMn-dhNFLn-aE6IcS3lpce_A2PB")
+	// if err != nil {
+	// 	t.Error(err)
+	// }
 	resp, err := csdk.AddTemplate("./template.odt", "")
 	if err != nil {
 		t.Error(err)
@@ -63,10 +76,33 @@ func TestAddDeleteTwiceTemplate(t *testing.T) {
 	if res.Success == false {
 		t.Error(res.Error)
 	}
-	res, _ = csdk.DeleteTemplate(resp.Data.TemplateID)
+	res, err = csdk.DeleteTemplate(resp.Data.TemplateID)
+	if err != nil {
+		t.Error(err)
+	}
 	if res.Success == true {
 		t.Error(errors.New("Error: the template should not be able to delete the template twice"))
 	}
+}
+
+func TestRenderTemplate(t *testing.T) {
+	templateID := "f90e67221d7d5ee11058a000bdb997fb41bf149b1f88b45cb1aba9edcab8f868"
+	cresp, err := csdk.RenderReport(templateID, `{"data":{"firstname":"Felix","lastname":"Arvid Ulf Kjellberg","color":"#00FF00"},"convertTo":"pdf"}`)
+	fmt.Printf("%+v", cresp)
+	if err != nil {
+		t.Error(err)
+	}
+	if cresp.Success == false {
+		t.Error(cresp.Error)
+	}
+	if len(cresp.Data.RenderID) <= 0 {
+		t.Error(errors.New("renderId has not been returned"))
+	}
+	fmt.Printf("%+v", cresp.Data)
+}
+
+func TestRenderAndGetReport(t *testing.T) {
+
 }
 
 // func TestGetTemplateAgain(t *testing.T) {
