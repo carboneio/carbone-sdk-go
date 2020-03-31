a4e1e6ca84389d31add10cdde764446602daa6c6
Steeve PAYRAUDEAU <spayraudeau@easilys.com>
2020-03-31 18:55:38 +0200

    Refactor GetTemplate AddTemplate

--- carbone.go
+++ carbone.go
@@ -10,7 +10,6 @@ import (
 	"mime/multipart"
 	"net/http"
 	"os"
-	"strconv"
 	"time"
 )
 
@@ -84,30 +83,21 @@ func (csdk *CSDK) AddTemplate(templateFileName string, payload string) (APIRespo
 	// Important if you do not close the multipart writer you will not have a terminating boundry
 	w.Close()
 	// Create the request
-	req, err := http.NewRequest("POST", csdk.apiURL+"/template", buf)
+	headerRequest := map[string]string{
+		"Content-Type": w.FormDataContentType(),
+	}
+	resp, err := csdk.doHTTPRequest("POST", csdk.apiURL+"/template", headerRequest, buf)
 	if err != nil {
-		return cResp, errors.New("Carbone SDK request: failled to create a new request: " + err.Error())
+		return cResp, err
 	}
-	// Set headers
-	req.Header.Set("Content-Type", w.FormDataContentType())
-	req.Header.Set("Authorization", csdk.apiAccessToken)
-	// Set client timeout
-	client := &http.Client{Timeout: csdk.apiTimeOut}
-	// Send request
-	resp, err := client.Do(req)
-	if err != nil {
-		return cResp, errors.New("Carbone SDK request error: " + strconv.Itoa(resp.StatusCode))
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return cResp, fmt.Errorf("Carbone SDK request error: status code %d", resp.StatusCode)
-	}
+	// Read the stream
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return cResp, errors.New("Carbone SDK request error: failled to read the body: " + err.Error())
 	}
-
+	// Close the connection
+	defer resp.Body.Close()
+	// Parse JSON body and store into the APIResponse Struct
 	err = json.Unmarshal(body, &cResp)
 	if err != nil {
 		return cResp, errors.New("Carbone SDK request error: failled to parse the JSON response from the body: " + err.Error())
@@ -117,24 +107,18 @@ func (csdk *CSDK) AddTemplate(templateFileName string, payload string) (APIRespo
 
 // GetTemplate returns the original template from the templateId (Unique identifier of the template)
 func (csdk *CSDK) GetTemplate(templateID string) ([]byte, error) {
-	req, err := http.NewRequest("GET", csdk.apiURL+"/template/"+templateID, nil)
+	// Create the request
+	resp, err := csdk.doHTTPRequest("GET", csdk.apiURL+"/template/"+templateID, nil, nil)
 	if err != nil {
-		return []byte{}, errors.New("Carbone SDK request: failled to create a new request: " + err.Error())
+		return []byte{}, err
 	}
-	req.Header.Set("Authorization", csdk.apiAccessToken)
-	// Set client timeout
-	client := &http.Client{Timeout: csdk.apiTimeOut}
-	// Send request
-	resp, err := client.Do(req)
-	if err != nil {
-		return []byte{}, fmt.Errorf("Carbone SDK request error: status code %d", resp.StatusCode)
-	}
-	defer resp.Body.Close()
 	// Read the response data and return a []byte. The http package automatically decodes chunking when reading response body.
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return []byte{}, errors.New("Carbone SDK request error: failled to read the body: " + err.Error())
 	}
+	// Close the connection
+	defer resp.Body.Close()
 	if len(body) == 0 {
 		return body, errors.New("Carbone SDK request error: The response body is empty")
 	}
@@ -233,7 +217,7 @@ func (csdk *CSDK) doHTTPRequest(method, url string, headers map[string]string,
 	body io.Reader) (*http.Response, error) {
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
-		return nil, err
+		return nil, errors.New("Carbone SDK request: failled to create a new request: " + err.Error())
 	}
 
 	for k, v := range headers {
@@ -253,11 +237,11 @@ func (csdk *CSDK) doHTTPRequest(method, url string, headers map[string]string,
 
 	// Send request
 	resp, err := csdk.apiHTTPClient.Do(req)
-
 	if err != nil {
-		return nil, fmt.Errorf("Carbone SDK request error: status code %d: %v", resp.StatusCode, err.Error())
+		return nil, fmt.Errorf("Carbone SDK request error: %v", err.Error())
 	}
-	defer resp.Body.Close()
-
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Carbone SDK request error: status code %d", resp.StatusCode)
+	}
 	return resp, nil
 }
