f51f95f4c602e3b32265b63072632367b4401f9f
Steeve PAYRAUDEAU <spayraudeau@easilys.com>
2020-04-03 17:07:17 +0200

    new test: render public report

--- carbone/carbone_test.go
+++ carbone/carbone_test.go
@@ -18,7 +18,6 @@ func TestMain(m *testing.M) {
 	}
 	status := m.Run()
 	deleteTmpFiles("tests")
-	deleteTmpFiles("")
 	os.Exit(status)
 }
 
@@ -398,6 +397,22 @@ func TestRender(t *testing.T) {
 		}
 		if len(report) > 0 {
 			t.Fatal(errors.New("Should have been empty"))
+		}
+	})
+
+	t.Run("Render the Carbone public report and change the access token.", func(t *testing.T) {
+		// t.Skip("Skip render public report test (because it change the access token)")
+		reportName := "./tests/homereport.tmp.test.pdf"
+		csdk.SetAccessToken("secret-token")
+		templateID := "template"
+		jsonData := `{"data":{"id":42,"date":1492012745,"company":{"name":"myCompany","address":"here","city":"Notfar","postalCode":123456},"customer":{"name":"myCustomer","address":"there","city":"Faraway","postalCode":654321},"products":[{"name":"product 1","priceUnit":0.1,"quantity":10,"priceTotal":1}],"total":140},"convertTo":"pdf"}`
+		reportBuffer, err := csdk.Render(templateID, jsonData, "")
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = ioutil.WriteFile(reportName, reportBuffer, 0644)
+		if err != nil {
+			t.Fatal(err)
 		}
 	})
 }
